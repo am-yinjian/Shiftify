759ef63f77a04d8b6b959e93a498ab226b77a3e1
am-yinjian <am.yinjian@gmal.com>
2025-08-14 18:24:53 -0500

    intialsetup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from "dotenv";
+import { conectDB } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = 5000;
+ 
+app.get("/products", (req, res)=> {});
+
+console.log(process.env.MONGO_URI);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server started at port ${PORT}`);
+
+});
