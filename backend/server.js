71540cd4b2c3bf23c4544edeabdae874a67ab5d6
am-yinjian <am.yinjian@gmal.com>
2025-08-14 19:19:52 -0500

    5002

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ const app = express();
 
 const PORT = 5002;
  
-app.get("/products", (req, res)=> {});
+app.get("/products", (req, res)=> {
+  res.send("Products endpoint is working!");
+  // res.render("poopy");
+});
+app.get("/", (req, res)=> {
+  res.send("Products endpoint is working!");
+});
 
 // console.log(process.env.MONGO_URI);
 
